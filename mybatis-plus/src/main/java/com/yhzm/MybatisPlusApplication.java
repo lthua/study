35934bba751ef28a4977bbe77603cdf0cd420e7e
lth <lth13553380334@163.com>
2020-03-24 17:48:56 +0800

    two commit

--- mybatis-plus/src/main/java/com/yhzm/MybatisPlusApplication.java
+++ mybatis-plus/src/main/java/com/yhzm/MybatisPlusApplication.java
@@ -1,9 +1,11 @@
 package com.yhzm;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.yhzm.dao")//扫描mapper文件
 public class MybatisPlusApplication {
 
     public static void main(String[] args) {
---
+++ mybatis-plus/src/main/java/com/yhzm/config/MybatisPlusConfig.java
@@ -0,0 +1,7 @@
+package com.yhzm.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MybatisPlusConfig {
+}
