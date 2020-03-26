b0142fa62743145b73928d7f8feb19b5665f0e6e
lth <lth13553380334@163.com>
2020-03-26 17:15:33 +0800

    第三次提交：测试成功

---
+++ mybatis-plus/src/test/java/com/yhzm/test/TestMP.java
@@ -0,0 +1,11 @@
+package com.yhzm.test;
+
+import com.yhzm.mapper.UserMapper;
+import com.yhzm.utils.SpringUtil;
+import org.apache.catalina.core.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestMP {
+
+    
+}
